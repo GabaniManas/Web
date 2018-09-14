180d211fd2b9bed11498bfe87aa6995ef36389f2
Manas Gabani <manasdineshgabani@gmail.com>
2018-09-14 09:42:55 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -18,17 +18,40 @@
 	</tr>
 </table>
 <button name="submit" type="submit">Comment</button>
-</form>
-</body>
-</html>
-<?php
+<button name="edit">Edit</button>
+</form><?php
 $con=mysqli_connect('localhost','root','','smss');
+if(!$con){
+	die("Connection Failed:" .mysqli_connect_error());
+}
 if(isset($_GET['submit']))
 {
 	
 	$message = $_GET['m'];
 	$uid=$_GET['uid'];
-$qry="INSERT INTO `comments`(`uid`,`message`) VALUES ('$uid','$message')";
-$run=mysqli_query($con,$qry);
-}
-?>
+	$run=mysqli_query($con,"INSERT INTO `comments`(`uid`,`message`) VALUES ('$uid','$message')");
+}	
+      ?>
+      <table border="2" style= "background-color: #84ed86; color: #761a9b; margin: 0 auto;" >
+      <thead>
+        <tr>
+          <th>id</th>
+          <th>comment</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php
+        $con=mysqli_connect('localhost','root','','smss');
+        $result = mysqli_query($con,"SELECT * FROM comments ");
+          while( $row = mysqli_fetch_assoc( $result ) ){
+            echo
+            "<tr>
+              <td>{$row['uid']}</td>
+              <td>{$row['message']}</td> 
+            </tr>";
+          }
+        ?>
+      </tbody>
+    </table>
+</body>
+</html>
